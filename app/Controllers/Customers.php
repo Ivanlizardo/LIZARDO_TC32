45a02b033f229bec26836a56ff4d09a0e9b088b5
Ivanlizardo <ivanmarklizardoup@gmail.com>
2026-09-12 02:04:28 +0800

    Initial commit

---
+++ app/Controllers/Customers.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controllers;
+
+class Customers extends BaseController
+{
+    public function index()
+    {
+        // $customers = [
+        //     [
+        //         'full_name' => 'Andrea Lucero',
+        //         'email' => 'andrea@gmail.com',
+        //         'phone' => '09123456789'
+        //     ],
+        //     [
+        //         'full_name' => 'Mandy Lizardo',
+        //         'email' => 'mandy@gmail.com',
+        //         'phone' => '09987654321'
+        //     ],
+        //     [
+        //         'full_name' => 'Ava Lucero',
+        //         'email' => 'ava@gmail.com',
+        //         'phone' => '09111222333'
+        //     ],
+        //     [
+        //         'full_name' => 'Ivan Lizardo',
+        //         'email' => 'ivan@gmail.com',
+        //         'phone' => '09222333444'
+        //     ],
+        //     [
+        //         'full_name' => 'Vandrea Lucerdo',
+        //         'email' => 'vandrea@gmail.com',
+        //         'phone' => '09333444555'
+        //     ]
+        // ];
+
+        // return view('customers', [
+        //     'customers' => $customers
+        // ]);
+
+        return view('customers/index');
+    }
+}
---
+++ app/Controllers/Users.php
@@ -0,0 +1,14 @@
+<?php
+
+    namespace App\Controllers;
+
+    class Users extends BaseController
+    {
+        public function index() {
+            return view('users/index');
+        }
+
+       
+    }
+
+
